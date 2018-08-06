0fd624259affb63542906a1ae2a7f79707bbd8ba
DESKTOP-GOL35J4\linhe <173661621@qq.com>
2018-08-06 11:09:49 +0800

    add auto sign keystore function.

---
+++ Assets/Editor/AutoSignKeystore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[InitializeOnLoad]
+public class AutoSignKeystore
+{
+
+    static AutoSignKeystore()
+    {
+        PlayerSettings.Android.keystoreName = "D:/keystore/luaframework.keystore";
+        PlayerSettings.Android.keystorePass = "luaframework";
+        PlayerSettings.Android.keyaliasName = "luafw";
+        PlayerSettings.Android.keyaliasPass = "luaframework";
+    }
+
+}
